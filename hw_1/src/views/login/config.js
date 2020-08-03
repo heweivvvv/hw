0de459a273aee886c59389581757a7c24176c840
heweivvvv <1198286396@qq.com>
2020-08-03 17:40:15 +0800

    asdfasf

--- hw_1/src/views/login/config.js
+++ hw_1/src/views/login/config.js
@@ -1,3 +1,3 @@
-export const routeId = 'login';
+export const stateId = 'login';
 
 
--- hw_1/src/views/login/container.jsx
+++ hw_1/src/views/login/container.jsx
@@ -5,56 +5,17 @@ import service from '../../service/helper'
 import Cookies from 'js-cookie'
 import {buildCheckCode} from '../../utils/util'
 import {connect} from 'react-redux';
+import {stateId} from "./config";
 
 class Login extends React.Component {
     constructor(props) {
         super(props);
         this.props = props;
-
-        this.state = {
-            userName: '',
-            password: '',
-            checkCode: '',
-            checkCodeText: buildCheckCode(),
-        }
     }
 
 
     async goLogin() {
-        try {
-            const params = {
-                loginName: this.state.userName,
-                password: this.state.password
-            };
-            if (!params.loginName) {
-                alert('请输入用户名');
-                return;
-            }
 
-            if (!params.password) {
-                alert('请输入密码');
-                return;
-            }
-
-            if (this.state.checkCodeText.toUpperCase() !== this.state.checkCode.toUpperCase()) {
-                alert('验证码错误');
-                this.setState({
-                    checkCodeText: buildCheckCode()
-                });
-                return;
-            }
-
-            let {user, msg, result} = await service('/signin', params, 'POST');
-            if (result) {
-                sessionStorage.setItem("isLogin", "1");
-                this.props.history.push('/home');
-                Cookies.set('userId', user.userId);
-            } else {
-                alert(msg);
-            }
-        } catch (e) {
-
-        }
     }
 
     changeUserName(e) {
@@ -89,12 +50,56 @@ class Login extends React.Component {
 }
 
 const mapStateToProps = (state) => {
+    console.log(state[stateId]);
+    const stateObj = state[stateId];
     return {
-
+        userName: stateObj.userName,
+        password: stateObj.password,
+        checkCode: stateObj.checkCode,
+        checkCodeText: stateObj.checkCodeText,
     }
 }
 
-const
+const mapDispatchToProps = (dispatch, props) => {
+    return {
+        async goLogin() {
+            try {
+                const params = {
+                    loginName: this.state.userName,
+                    password: this.state.password
+                };
+                if (!params.loginName) {
+                    alert('请输入用户名');
+                    return;
+                }
+
+                if (!params.password) {
+                    alert('请输入密码');
+                    return;
+                }
+
+                if (this.state.checkCodeText.toUpperCase() !== this.state.checkCode.toUpperCase()) {
+                    alert('验证码错误');
+                    this.setState({
+                        checkCodeText: buildCheckCode()
+                    });
+                    return;
+                }
+
+                let {user, msg, result} = await service('/signin', params, 'POST');
+                if (result) {
+                    sessionStorage.setItem("isLogin", "1");
+                    this.props.history.push('/home');
+                    Cookies.set('userId', user.userId);
+                } else {
+                    alert(msg);
+                }
+            } catch (e) {
+
+            }
+        }
+    }
+}
 
 
 export default withRouter(Login);
